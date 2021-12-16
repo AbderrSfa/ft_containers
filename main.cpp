997179eac8df652ebbaf46cf8c2dd86dd6886c9c
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2021-12-16 11:03:37 +0100

    Setup for working on vector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <vector>
 #include <stack>
 #include <map>
+#include <iostream>
 
 int		main() {
+	std::vector<int>	v;
+	for (size_t i = 1; i < 8; i++)
+	{
+		v.push_back(i);
+	}
+	
+	std::cout << "v.begin() => " << *v.begin() << std::endl;
+	std::cout << "v.end() => " << *v.end() << std::endl;
 	return (0);
 }
