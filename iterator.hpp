7e250747eaf66828d1184dee2ab713318d2032f4
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-01-13 17:32:17 +0100

    Removed integers class + some old comments

--- iterator.hpp
+++ iterator.hpp
@@ -5,11 +5,12 @@
 
 namespace ft
 {
-	template <	class Category,
-				class T,
-				class Distance = std::ptrdiff_t,
-				class Pointer = T*,
-				class Reference = T& >
+	template <
+		class Category,
+		class T,
+		class Distance = std::ptrdiff_t,
+		class Pointer = T*,
+		class Reference = T& >
 	class iterator
 	{
 		typedef T			value_type;
@@ -17,21 +18,6 @@ namespace ft
 		typedef	Pointer		pointer;
 		typedef	Reference	reference;
 		typedef	Category	iterator_category;
-
-		public:
-			iterator(pointer ptr) : m_ptr(ptr) {}
-
-			reference	operator*() const { return *m_ptr; }
-			pointer		operator->() { return m_ptr; }
-
-			iterator&	operator++() { m_ptr++; return (*this); }
-			iterator	operator++(int) { iterator	tmp = *this; ++(*this); return tmp; }
-
-			friend bool operator== (const iterator& a, const iterator& b) { return a.m_ptr == b.m_ptr; };
-			friend bool operator!= (const iterator& a, const iterator& b) { return a.m_ptr != b.m_ptr; };
-			
-		private:
-			pointer	m_ptr;
 	};
 
 	template <class Iterator>
@@ -52,16 +38,6 @@ namespace ft
 		typedef T*								pointer;
 		typedef T&								reference;
 		typedef std::random_access_iterator_tag iterator_category;
-	};
-
-	template < class T >
-	class   integers {
-		private:
-			T m_data[200];
-
-		public:
-			iterator<T, std::forward_iterator_tag>	begin() { return iterator<T, std::forward_iterator_tag>(&m_data[0]); }
-			iterator<T, std::forward_iterator_tag>	end() { return iterator<T, std::forward_iterator_tag>(&m_data[200]); }
 	};
 }
 
--- main.cpp
+++ main.cpp
@@ -3,18 +3,6 @@
 #include <iostream>
 
 int     main() {
-/*    ft::integers<int>    numbers;
-
-	std::fill(numbers.begin(), numbers.end(), 4);
-	ft::iterator<int, std::forward_iterator_tag> begin = numbers.begin();
-	ft::iterator<int, std::forward_iterator_tag> end = numbers.end();
-	while (begin != end)
-	{
-		std::cout << *begin << "\n";
-		begin++;
-	}*/
-	ft::integers<int>	numbers;
-	ft::iterator_traits<ft::iterator<int, std::forward_iterator_tag> >	it;
+	ft::iterator<int, std::forward_iterator_tag>	it;
 	(void)it;
-	(void)numbers;
 }
