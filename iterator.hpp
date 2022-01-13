8b4bfd8dac57550da09ef42db0cd6098772e9cba
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-01-13 19:34:17 +0100

    Tiny change

--- iterator.hpp
+++ iterator.hpp
@@ -11,7 +11,7 @@ namespace ft
 		class Distance = std::ptrdiff_t,
 		class Pointer = T*,
 		class Reference = T& >
-	class iterator
+	struct iterator
 	{
 		typedef T			value_type;
 		typedef Distance	difference_type;
@@ -21,17 +21,27 @@ namespace ft
 	};
 
 	template <class Iterator>
-	class iterator_traits
+	struct iterator_traits
 	{
-		typedef typename Iterator::difference_type difference_type;
-		typedef typename Iterator::value_type value_type;
-		typedef typename Iterator::pointer pointer;
-		typedef typename Iterator::reference reference;
-		typedef typename Iterator::iterator_category iterator_category;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+		typedef typename Iterator::iterator_category	iterator_category;
 	};
 
 	template <class T>
-	class iterator_traits<T*>
+	struct iterator_traits<T*>
+	{
+		typedef std::ptrdiff_t					difference_type;
+		typedef T 								value_type;
+		typedef T*								pointer;
+		typedef T&								reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+
+	template <class T>
+	struct iterator_traits<const T*>
 	{
 		typedef std::ptrdiff_t					difference_type;
 		typedef T 								value_type;
