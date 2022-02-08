2ed8a798a5b16bbb65d3ad0623419ca3a7755950
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-02-08 17:05:16 +0100

    Added tree balancing and rotations (still have some errors)

--- AVLTry.cpp
+++ AVLTry.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 template <class T>
 class Node {
@@ -10,7 +11,9 @@ public:
 
 	Node(T obj) {
 		data = obj;
-		parent = left = right = NULL;
+		parent = NULL;
+		left = NULL;
+		right = NULL;
 	};
 
 };
@@ -21,8 +24,7 @@ private:
 	Node<T>*	root;
 	size_t		CurrentSize;
 
-	int maxHeight(Node<T>* node)
-	{
+	int maxHeight(Node<T>* node) {
 		if (node == NULL)
 			return -1;
 		else
@@ -35,10 +37,9 @@ private:
 			else
 				return(rDepth + 1);
 		}
-	}
+	};
 	
-	void	print2DUtil(Node<T>* root, int space)
-	{
+	void	print2DUtil(Node<T>* root, int space) {
 		if (root == NULL)
 			return;
 		space += 10;
@@ -46,19 +47,85 @@ private:
 		std::cout << std::endl;
 		for (int i = 10; i < space; i++)
 			std::cout << " ";
-		std::cout << "{" << root->data << "} " << maxHeight(root) << "\n";
+		std::cout << "{" << root->data << "} " << abs(maxHeight(root->left) - maxHeight(root->right)) << "\n";
 		print2DUtil(root->left, space);
 	};
+
+	Node<T>* rightLeftRotate(Node<T>* node) {
+		node->right = rightRotate(node->right);
+		return (leftRotate(node));
+	};
+
+	Node<T>* leftRightRotate(Node<T>* node) {
+		node->left = leftRotate(node->left);
+		return (rightRotate(node));
+	};
+
+	Node<T>* leftRotate(Node<T>* node) {
+		Node<T>* temp = node->right;
+		node->right = temp->left;
+		temp->left = node;
+		temp->parent = node->parent;
+		if (node->parent) {
+			if (node == node->parent->right)
+				node->parent->right = temp;
+			else
+				node->parent->left = temp;
+		}
+		return (temp);
+	};
+
+	Node<T>* rightRotate(Node<T>* node) {
+		Node<T>* temp = node->left;
+		node->left = temp->right;
+		temp->right = node;
+		temp->parent = node->parent;
+		if (node->parent) {
+			if (node == node->parent->right)
+				node->parent->right = temp;
+			else
+				node->parent->left = temp;
+		}
+		return (temp);
+	};
+
+	void	rebalanceTree(Node<T>* node) {
+		if (maxHeight(node->left) - maxHeight(node->right) > 1) {
+			if (maxHeight(node->left->left) > maxHeight(node->left->right)) {
+				std::cout << "right rotation\n";
+				node = rightRotate(node);
+			}
+			else {
+				std::cout << "left-right rotation\n";
+				node = leftRightRotate(node);
+			}
+		}
+		else {
+			if (maxHeight(node->right->right) > maxHeight(node->right->left)) {
+				std::cout << "left rotation\n";
+				node = leftRotate(node);
+			}
+			else {
+				std::cout << "right-left rotation\n";
+				node = rightLeftRotate(node);
+			}
+		}
+		if (node->parent == NULL)
+			this->root = node;
+		//print2DUtil(node, 0);
+	};
+
 	void	checkBalance(Node<T>* node) {
 		if ((maxHeight(node->left) - maxHeight(node->right) > 1)
 			|| (maxHeight(node->left) - maxHeight(node->right) < -1))
 		{
-			std::cout << "needs rebalancing\n";
+			rebalanceTree(node);
 		}
 		if (node->parent == NULL)
 			return;
 		checkBalance(node->parent);
 	};
+
 	void	add(Node<T>* parent, Node<T>* newNode) {
 		if (newNode->data > parent->data) {
 			if (parent->right == NULL) {
@@ -108,12 +175,26 @@ public:
 int		main() {
 	AVLTree<int>	tree;
 
-	tree.add(5);
+
 	tree.add(10);
+	tree.add(4);
+	tree.add(6);
 	tree.add(1);
-	tree.add(18);
-	tree.add(16);
-	tree.add(11);
+	tree.add(3);
+	// tree.add(9);
+	// tree.add(8);
+	// tree.add(7);
+	// tree.add(5);
+	// tree.add(2);
+	// tree.add(52);
+	// tree.add(38);
+	// tree.add(25);
+	// tree.add(40);
+	// tree.add(61);
+	// tree.add(74);
+	// tree.add(23);
+	// tree.add(35);
+	// tree.add(68);
 	tree.printTree();
 	return (0);
 }
