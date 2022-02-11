457c2ea5b8b3dd7befe0bb494af1a77e1864077f
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-02-11 16:47:49 +0100

    AVL now using key and value + Working on AVL delete

--- AVLPair.cpp
+++ AVLPair.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <cstdlib>
 
-template <class T>
+template <class K, class V>
 class Node {
 public:
-	T		data;
-	Node*	parent;
-	Node*	left;
-	Node*	right;
+	K		key;
+	V		value;
+	Node* parent;
+	Node* left;
+	Node* right;
 
-	Node(T obj) {
-		data = obj;
+	Node(K objKey, V objVal) {
+		key = objKey;
+		value = objVal;
 		parent = NULL;
 		left = NULL;
 		right = NULL;
 	};
 };
 
-template <class T>
+template <class K, class V>
 class AVLTree {
 private:
-	Node<T>*	root;
+	Node<K, V>* root;
 	size_t		CurrentSize;
 
-	int maxHeight(Node<T>* node) {
+	int maxHeight(Node<K, V>* node) {
 		if (node == NULL)
 			return -1;
 		else
@@ -38,7 +40,7 @@ private:
 		}
 	};
 	
-	void	print2DUtil(Node<T>* root, int space) {
+	void	print2DUtil(Node<K, V>* root, int space) {
 		if (root == NULL)
 			return;
 		space += 10;
@@ -46,22 +48,22 @@ private:
 		std::cout << std::endl;
 		for (int i = 10; i < space; i++)
 			std::cout << " ";
-		std::cout << "{" << root->data << "} " << abs(maxHeight(root->left) - maxHeight(root->right)) << "\n";
+		std::cout << "{" << root->key << " " << root->value << "} " << abs(maxHeight(root->left) - maxHeight(root->right)) << "\n";
 		print2DUtil(root->left, space);
 	};
 
-	Node<T>* rightLeftRotate(Node<T>* node) {
+	Node<K, V>* rightLeftRotate(Node<K, V>* node) {
 		node->right = rightRotate(node->right);
 		return (leftRotate(node));
 	};
 
-	Node<T>* leftRightRotate(Node<T>* node) {
+	Node<K, V>* leftRightRotate(Node<K, V>* node) {
 		node->left = leftRotate(node->left);
 		return (rightRotate(node));
 	};
 
-	Node<T>* leftRotate(Node<T>* node) {
-		Node<T>* temp = node->right;
+	Node<K, V>* leftRotate(Node<K, V>* node) {
+		Node<K, V>* temp = node->right;
 		node->right = temp->left;
 		temp->left = node;
 		temp->parent = node->parent;
@@ -76,8 +78,8 @@ private:
 		return (temp);
 	};
 
-	Node<T>* rightRotate(Node<T>* node) {
-		Node<T>* temp = node->left;
+	Node<K, V>* rightRotate(Node<K, V>* node) {
+		Node<K, V>* temp = node->left;
 		node->left = temp->right;
 		temp->right = node;
 		temp->parent = node->parent;
@@ -91,25 +93,25 @@ private:
 			node->parent = temp;
 		return (temp);
 	};
-
-	void	rebalanceTree(Node<T>* node) {
+	
+	void	rebalanceTree(Node<K, V>* node) {
 		if (maxHeight(node->left) - maxHeight(node->right) > 1) {
 			if (maxHeight(node->left->left) > maxHeight(node->left->right)) {
-				std::cout << "right rotation\n";
+				//std::cout << "right rotation\n";
 				node = rightRotate(node);
 			}
 			else {
-				std::cout << "left-right rotation\n";
+				//std::cout << "left-right rotation\n";
 				node = leftRightRotate(node);
 			}
 		}
 		else {
 			if (maxHeight(node->right->right) > maxHeight(node->right->left)) {
-				std::cout << "left rotation\n";
+				//std::cout << "left rotation\n";
 				node = leftRotate(node);
 			}
 			else {
-				std::cout << "right-left rotation\n";
+				//std::cout << "right-left rotation\n";
 				node = rightLeftRotate(node);
 			}
 		}
@@ -117,7 +119,7 @@ private:
 			this->root = node;
 	};
 
-	void	checkBalance(Node<T>* node) {
+	void	checkBalance(Node<K, V>* node) {
 		if ((maxHeight(node->left) - maxHeight(node->right) > 1)
 			|| (maxHeight(node->left) - maxHeight(node->right) < -1))
 		{
@@ -128,8 +130,8 @@ private:
 		checkBalance(node->parent);
 	};
 
-	void	add(Node<T>* parent, Node<T>* newNode) {
-		if (newNode->data > parent->data) {
+	void	add(Node<K, V>* parent, Node<K, V>* newNode) {
+		if (newNode->key > parent->key) {
 			if (parent->right == NULL) {
 				parent->right = newNode;
 				newNode->parent = parent;
@@ -153,6 +155,45 @@ private:
 		checkBalance(newNode);
 	};
 
+	bool	search(Node<K, V>* node, K key) {
+		if (node == NULL)
+			return false;
+		else if (node->key == key)
+			return true;
+		else if (key < node->key)
+			return search(node->left, key);
+		else
+			return search(node->right, key);			
+	};
+
+	Node<K,V>*	deleteNode(Node<K, V>* node, K key) {
+		if (node == NULL)
+			return NULL;
+		else if (key < node->key)
+			node->left = deleteNode(node->left, key);
+		else if (key > node->key)
+			node->right = deleteNode(node->right, key);
+		else
+		{
+			std::cout << "Gotcha: " << node->value << std::endl;
+			if (node->left == NULL && node->right == NULL) {
+				delete node;
+				node = NULL;
+				return node;
+				std::cout << "No children\n";
+			}
+			else if (node->left == NULL) {
+				std::cout << "One right child\n";				
+			}
+			else if (node->right == NULL) {
+				std::cout << "One left child\n";
+			}
+			else {
+				std::cout << "two children\n";
+			}
+		}
+	};
+
 public:
 	AVLTree() {
 		root = NULL;
@@ -165,8 +206,8 @@ public:
 		std::cout << "||||||||||||||||\n\n";
 	};
 
-	void	add(T obj) {
-		Node<T>* node = new Node<T>(obj);
+	void	add(K objKey, V objVal) {
+		Node<K, V>* node = new Node<K, V>(objKey, objVal);
 		if (this->root == NULL) {
 			this->root = node;
 			this->CurrentSize++;
@@ -174,40 +215,29 @@ public:
 		}
 		add(this->root, node);
 	};
+
+	bool	search(K key) {
+		return (search(this->root, key));
+	};
+
+	void	deleteNode(K key) {
+		deleteNode(this->root, key);
+	};
 };
 
 int		main() {
-	AVLTree<int>	tree;
+	AVLTree<int, char>	tree;
 
-	tree.add(52);
-	tree.add(38);
-	tree.add(25);
-	tree.add(40);
-	tree.add(61);
-	tree.add(74);
-	tree.add(23);
-	tree.add(35);
-	tree.add(68);
-	//tree.add(57);
-	//tree.add(65);
-	//tree.add(53);
-	//tree.add(34);
-	//tree.add(18);
-	//tree.add(88);
-	//tree.add(80);
-	//tree.add(67);
-	//tree.add(57);
-	//tree.add(37);
-	//tree.add(85);
-	//tree.add(17);
-	//tree.add(36);
-	//tree.add(10);
-	//tree.add(2);
-	//tree.add(35);
-	//tree.add(3);
-	//tree.add(89);
-	//tree.add(59);
-	//tree.add(50);
+	tree.add(38, 'b');
+	tree.add(52, 'a');
+	tree.add(25, 'c');
+	tree.add(40, 'd');
+	tree.add(61, 'e');
+	tree.add(74, 'f');
+	tree.add(35, 'h');
+	tree.add(68, 'i');
+	//tree.deleteNode(74);
+	//std::cout << tree.search(53) << std::endl;
 	tree.printTree();
 	return (0);
 }
